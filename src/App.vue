f0b1b3b52dcde9cf1316ce855a401837d4300e54
Nelson Widjaya <nelson.widjaya@gmail.com>
2020-07-01 01:09:46 +0900

    #1 add some module that might be useful

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,35 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ButtonsObject num=1 />
+    <ButtonsObject num=2 />
+    <ButtonsObject num=3 />
+    <ButtonsObject num=4 />
+    <ButtonsObject num=5 />
+    <ButtonsObject num=6 />
+    <ButtonsObject num=7 />
+    <ButtonsObject num=8 />
+
+    <half-circle-spinner
+  :animation-duration="1000"
+  :size="300"
+  color="#000000"
+/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { HalfCircleSpinner } from 'epic-spinners'
 
+import HelloWorld from './components/HelloWorld.vue'
+import ButtonsObject from './components/ButtonsObject.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ButtonsObject,
+    HalfCircleSpinner
   }
 }
 </script>
---
+++ src/components/ButtonsObject.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="buttons">
+    
+    <vs-button>{{num}}</vs-button>
+    
+  </div>
+</template>
+
+<script>
+import { vsButton, vsSelect, vsPopup } from 'vuesax'
+import 'vuesax/dist/vuesax.css'
+import Vue from 'vue'
+Vue.use(vsButton);
+Vue.use(vsSelect);
+Vue.use(vsPopup);
+
+export default {
+  name: 'ButtonsObject',
+  props: {
+    num:String
+  },
+  methods:{
+      
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
